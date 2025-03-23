0f5290716ad95408c6ffb3a9cb3ea8df6d097235
mshel <maryna.shelenkova@gmx.de>
2025-03-23 14:26:15 +0100

    add HA2

---
+++ HA5/HA5.py
@@ -0,0 +1,17 @@
+from langchain.document_loaders import WebBaseLoader
+from langchain.chains.summarize import load_summarize_chain
+from langchain.llms import OpenAI
+
+
+def summarize_url(url):
+    loader = WebBaseLoader(url)
+    docs = loader.load()
+    llm = OpenAI(temperature=0)
+    chain = load_summarize_chain(llm, chain_type="map_reduce")
+    summary = chain.run(docs)
+    return summary
+
+
+url = "https://itcareerhub.de/"
+summary = summarize_url(url)
+print(summary)
